fd09ac3b1f501a1aa9163c1e244c067b0bd083e0
Саша <7949894@ukr.net>
2022-09-17 00:16:12 +0300

    Into the main class added report screen output for new user with deposit.

--- src/HW_3/Main.java
+++ src/HW_3/Main.java
@@ -2,5 +2,7 @@ package HW_3;
 
 public class Main {
     public static void main(String[] args) {
+        Mono depositCalculator = new Mono(new User("Alexandr", "Voskresenskyy"), 10_000);
+        depositCalculator.printReport();
     }
 }
