f187ae7739b952d859692b521aff8e5ab0aa12b0
ovoskresenskyy <7949894@ukr.net>
2022-09-21 13:19:50 +0300

    Added solution for the second task - method checkSumSign
    Parameters a & b got by taking random from minValue to maxValue

--- src/HW_4/HomeWorkApp.java
+++ src/HW_4/HomeWorkApp.java
@@ -3,6 +3,7 @@ package HW_4;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     static void printThreeWords() {
@@ -12,4 +13,15 @@ public class HomeWorkApp {
                 Apple""");
     }
 
+    static void checkSumSign() {
+        int minValue = -100;
+        int maxValue = 100;
+
+        int a = (int) ((Math.random() * (maxValue - minValue)) + minValue);
+        int b = (int) ((Math.random() * (maxValue - minValue)) + minValue);
+        int sum = a + b;
+
+        System.out.println("a + b = " + sum);
+        System.out.println("Sum of a and b is " + (sum >= 0 ? "positive" : "negative"));
+    }
 }
