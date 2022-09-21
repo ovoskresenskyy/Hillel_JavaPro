f8ded9cdb2a1b8fddaa9541d2922559479a94682
ovoskresenskyy <7949894@ukr.net>
2022-09-21 13:17:06 +0300

    Added solution for the first task - method printThreeWords

--- src/HW_4/HomeWorkApp.java
+++ src/HW_4/HomeWorkApp.java
@@ -2,6 +2,14 @@ package HW_4;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-
+        printThreeWords();
     }
+
+    static void printThreeWords() {
+        System.out.print("""
+                Orange
+                Banana
+                Apple""");
+    }
+
 }
