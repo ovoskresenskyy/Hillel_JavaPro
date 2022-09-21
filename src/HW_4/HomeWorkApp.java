04e9de90815ce144d7feb5542be4d164ac16d563
ovoskresenskyy <7949894@ukr.net>
2022-09-21 13:32:39 +0300

    Added solution for the third task - method printColor
    Added dividing line for each method.

--- src/HW_4/HomeWorkApp.java
+++ src/HW_4/HomeWorkApp.java
@@ -4,13 +4,15 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
     }
 
     static void printThreeWords() {
-        System.out.print("""
+        System.out.println("""
                 Orange
                 Banana
-                Apple""");
+                Apple
+                """);
     }
 
     static void checkSumSign() {
@@ -21,7 +23,23 @@ public class HomeWorkApp {
         int b = (int) ((Math.random() * (maxValue - minValue)) + minValue);
         int sum = a + b;
 
-        System.out.println("a + b = " + sum);
-        System.out.println("Sum of a and b is " + (sum >= 0 ? "positive" : "negative"));
+        System.out.println("a + b = " + sum + ". Sum is " + (sum >= 0 ? "positive" : "negative"));
+        System.out.println();
+    }
+
+    static void printColor() {
+        int minValue = -200;
+        int maxValue = 200;
+
+        int value = (int) ((Math.random() * (maxValue - minValue)) + minValue);
+
+        String color;
+
+        if (value <= 0) color = "Red";
+        else if (value > 100) color = "Green";
+        else color = "Yellow";
+
+        System.out.println("value = " + value + ". Match color - " + color);
+        System.out.println();
     }
 }
