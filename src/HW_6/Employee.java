41c57e9e0e9704a030e922f3d8b04a52224045d2
ovoskresenskyy <7949894@ukr.net>
2022-10-01 21:51:32 +0300

    Field fullName convert to uppercase

--- src/HW_6/Employee.java
+++ src/HW_6/Employee.java
@@ -2,22 +2,22 @@ package HW_6;
 
 public class Employee {
 
-    private final String fullName;
+    private final String FULL_NAME;
     private String position;
     private String email;
     private int phoneNumber;
     private int age;
 
     public Employee(String name, String surname, String position, String email, int phoneNumber, int age) {
-        this.fullName = name + " " + surname;
+        this.FULL_NAME = name + " " + surname;
         this.position = position;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.age = age;
     }
 
-    public String getFullName() {
-        return fullName;
+    public String getFULL_NAME() {
+        return FULL_NAME;
     }
 
     public String getPosition() {
