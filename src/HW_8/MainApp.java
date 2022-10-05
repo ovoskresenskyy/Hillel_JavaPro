09879e9a809b79913022e390aefcecc2cdaedf2f
ovoskresenskyy <7949894@ukr.net>
2022-10-05 23:44:31 +0300

    Added simple solution for HW 8.

--- src/HW_8/MainApp.java
+++ src/HW_8/MainApp.java
@@ -1,7 +1,25 @@
 package HW_8;
 
+import java.util.Arrays;
+
 public class MainApp {
     public static void main(String[] args) {
 
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(runningSum(nums)));
+
+        nums = new int[]{1, 1, 1, 1, 1};
+        System.out.println(Arrays.toString(runningSum(nums)));
+
+        nums = new int[]{3, 1, 2, 10, 1};
+        System.out.println(Arrays.toString(runningSum(nums)));
+
+    }
+
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
     }
 }
