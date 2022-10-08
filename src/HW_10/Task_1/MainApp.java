6860521757c71acab14df0fa489661a4a63d2b85
ovoskresenskyy <7949894@ukr.net>
2022-10-08 12:29:47 +0300

    Added occurrence counting in Array list Used Collection. frequency

--- src/HW_10/Task_1/MainApp.java
+++ src/HW_10/Task_1/MainApp.java
@@ -1,7 +1,18 @@
 package HW_10.Task_1;
 
+import java.util.*;
+
 public class MainApp {
     public static void main(String[] args) {
 
+        String[] nationalSalute = "Glory to Ukraine! Glory to the heroes! Glory of the nation! Death to the enemies!".split(" ");
+
+        List<String> listOfWords = new ArrayList<>(Arrays.asList(nationalSalute));
+        System.out.println(countOccurrence(listOfWords, "Glory"));
+
+    }
+
+    public static int countOccurrence(List<String> incomeList, String occurrence) {
+        return Collections.frequency(incomeList, occurrence);
     }
 }
