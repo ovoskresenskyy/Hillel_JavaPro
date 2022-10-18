6d50f5e261261834975ae5667c4365ea884046b1
ovoskresenskyy <7949894@ukr.net>
2022-10-18 21:21:09 +0300

    Changed to simple code, for not use lambda.

--- src/HW_11/CoffeeOrderBoard.java
+++ src/HW_11/CoffeeOrderBoard.java
@@ -22,20 +22,18 @@ public class CoffeeOrderBoard {
     }
 
     public Order deliver(int orderNumber) {
-        if (orderList.isEmpty()) return null;
 
+        if (orderList.isEmpty()) return null;
         Order orderToDeliver = null;
 
         int left = 0;
         int right = orderList.size() - 1;
         while (left <= right) {
-            int middle = left + (right - left) / 2;
 
-            if (orderList.get(middle).getNumber() > orderNumber) {
-                right = middle - 1;
-            } else if (orderList.get(middle).getNumber() < orderNumber) {
-                left = middle + 1;
-            } else {
+            int middle = left + (right - left) / 2;
+            if (orderList.get(middle).getNumber() > orderNumber) right = middle - 1;
+            else if (orderList.get(middle).getNumber() < orderNumber) left = middle + 1;
+            else {
                 orderToDeliver = orderList.remove(middle);
                 break;
             }
@@ -44,10 +42,13 @@ public class CoffeeOrderBoard {
     }
 
     public void draw() {
+
         StringBuilder textToPrint = new StringBuilder();
         textToPrint.append("-----------\n Num | Name\n-----------\n");
 
-        orderList.forEach(textToPrint::append);
+        for (Order order : orderList) {
+            textToPrint.append(order);
+        }
 
         System.out.println(textToPrint);
     }
