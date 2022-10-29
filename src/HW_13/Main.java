dd21a1272859235e76f7870dfeeca7674ab8da1a
ovoskresenskyy <7949894@ukr.net>
2022-10-29 03:15:05 +0300

    Added solution for HW  - used recursion to find file

--- src/HW_13/Main.java
+++ src/HW_13/Main.java
@@ -1,7 +1,21 @@
 package HW_13;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        FileProcessing bookParser = new FileProcessing();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please insert book name to search: ");
+
+        File book = bookParser.findFileByName(scanner.nextLine());
+        if (book.isFile()) {
+            bookParser.parseBook(book);
+        } else {
+            System.out.println("File with that name doesn't exist.");
+        }
     }
 }
