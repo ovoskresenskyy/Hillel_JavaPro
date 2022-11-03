0f4379d09aa1af1ad690725770497f24e52fe2f8
ovoskresenskyy <7949894@ukr.net>
2022-11-03 15:01:05 +0200

    Trying stream

---
+++ src/HW_4/StreamImpl/Main.java
@@ -0,0 +1,10 @@
+package HW_4.StreamImpl;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StreamDemo streamDemo = new StreamDemo();
+        streamDemo.printThreeWords();
+        streamDemo.checkSumSign();
+    }
+}
